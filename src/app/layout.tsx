323b163061295b71e6bf73ef6aec3f60247fad8e
Erol Mehmed <erol.mehmed07@gmail.com>
2026-06-09 23:07:33 +0300

    feat: enhance header and grid styles with hover and active states, and add accent-active variable

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,8 @@
+import '@/styles/globals.scss';
 import type { Metadata } from 'next';
 import { StoreProvider } from '@/store/provider';
 import React from 'react';
 import Header from '@/components/layout/Header';
-import '@/styles/globals.scss';
 import Footer from '@/components/layout/Footer';
 
 export const metadata: Metadata = {
