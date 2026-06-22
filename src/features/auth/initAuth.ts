40f3bba05358da96a27285cd6854b2d5b068c7f0
Erol Mehmed <erol.mehmed07@gmail.com>
2026-06-22 18:16:26 +0300

    feat: add authentication initialization and update API configuration for credentials

---
+++ src/features/auth/initAuth.ts
@@ -0,0 +1,18 @@
+import { api } from '@/shared/services/api';
+import { AppDispatch } from '@/store/store';
+import { logout, setCredentials } from '@/features/auth/authSlice';
+
+export const initializeAuth = async (dispatch: AppDispatch) => {
+  try {
+    const { data } = await api.post('/auth/refresh');
+
+    dispatch(
+      setCredentials({
+        access_token: data.access_token,
+        user: data.user,
+      }),
+    );
+  } catch {
+    dispatch(logout());
+  }
+};
--- src/store/provider.tsx
+++ src/store/provider.tsx
@@ -2,12 +2,20 @@
 
 import { Provider } from 'react-redux';
 import { store } from '@/store/store';
-import React from 'react';
+import React, { useEffect } from 'react';
+import { initializeAuth } from '@/features/auth/initAuth';
+import { useAppDispatch } from '@/store/hooks';
 
 interface StoreProviderProps {
   children: React.ReactNode;
 }
 
 export const StoreProvider = ({ children }: StoreProviderProps) => {
+  const dispatch = useAppDispatch();
+
+  useEffect(() => {
+    initializeAuth(dispatch).then((r) => r);
+  }, []);
+
   return <Provider store={store}>{children}</Provider>;
 };
